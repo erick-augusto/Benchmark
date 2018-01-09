be2378626453bfbe4834de7c21ed6b66f53a7459
erick-augusto <erick_aug@hotmail.com>
2018-01-09 11:18:57 -0200

    Initial Commit

--- include/RandomCL.h
+++ include/RandomCL.h
@@ -2,23 +2,5 @@
 #define RANDOMCL_H
 
 template<typename Tipo>
-class RandomCL{
-    private:
-        Tipo r;
-        unsigned char i;
-    public:
-        RandomCL(unsigned char i) {
-            this->i= i;
-            reset();
-        }
-        Tipo nextr() {
-            r = 17*r + 23;
-            return(r);
-        }
-        void reset() {
-            unsigned short seeds[6] = {4, 81, 151, 1601, 2307, 4207};
-            r = seeds[i%6];
-        }
-};
 
 #endif
