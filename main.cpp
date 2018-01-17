2a49b3ceb045081e23cf1c0eec5785ff9be17e9c
Erick <erick_aug@hotmail.com>
2018-01-17 15:12:21 -0200

    Variáveis Iniciais

--- main.cpp
+++ main.cpp
@@ -13,4 +13,12 @@ int compare(const void * a, const void * b){
     if ( *(unsigned int*)a >  *(unsigned int*)b ) return 1;
 }
 
+int main()
+{
+    int i, j, k, n, t1, t2;
+    unsigned long int *vet;
+    int tamanho[9] = {500, 1000, 2500, 5000, 7500, 10000, 15000, 20000, 25000};
+    double tempo[6];
+    double media[9];
 
+}
