d82a8b0b52718e60f5393285da6473b28a088e94
Erick <erick_aug@hotmail.com>
2018-01-25 20:34:26 -0200

    Verifica o maior

--- include/Ordenacao.h
+++ include/Ordenacao.h
@@ -22,7 +22,11 @@ namespace ED {
 		int fl, fr, imaior;
         fl = (pai << 1) + 1;
         fr = fl + 1;
-		
+		while (true) {
+            if ((fl < heapsize) && (vet[fl] > vet[pai])) imaior = fl;
+            else imaior = pai;
+            if ((fr < heapsize) && (vet[fr] > vet[imaior])) imaior = fr;
+		}
 	}
 	
 	template <typename T>
