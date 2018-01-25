36f365aa230e74d102a0298cf1e5b92fae0f865c
Erick <erick_aug@hotmail.com>
2018-01-25 20:57:04 -0200

    Ordenando posições

--- include/Ordenacao.h
+++ include/Ordenacao.h
@@ -17,16 +17,16 @@ namespace ED {
             static void merge(T *vet, T *aux, int esq, int meio, int dir);
     };
 	
-	template <typename T>
+    template <typename T>
     void Ordenacao<T>::heapify(T *vet, int pai, int heapsize) {
-		int fl, fr, imaior;
+        int fl, fr, imaior;
         fl = (pai << 1) + 1;
         fr = fl + 1;
-		while (true) {
+        while (true) {
             if ((fl < heapsize) && (vet[fl] > vet[pai])) imaior = fl;
             else imaior = pai;
             if ((fr < heapsize) && (vet[fr] > vet[imaior])) imaior = fr;
-			if (imaior != pai){
+            if (imaior != pai){
                 T aux = vet[pai];
                 vet[pai] = vet[imaior];
                 vet[imaior] = aux;
@@ -35,14 +35,20 @@ namespace ED {
                 fr = fl + 1;
             }
             else break;
-		}
-	}
-	
-	template <typename T>
+        }
+    }
+
+    template <typename T>
     void Ordenacao<T>::heapsort(T *vet, int n) {
         int i;
-		for (i=(n>>1)-1; i>=0; i--) heapify(vet, i, n);
-	}
+        for (i=(n>>1)-1; i>=0; i--) heapify(vet, i, n);
+        for (i=n-1; i > 0; i--) {
+            T aux = vet[0];
+            vet[0] = vet[i];
+            vet[i] = aux;
+            heapify(vet, 0, i);
+        }
+    }
 	
 	template <typename T>
     void Ordenacao<T>::quicks_central(T *vet, int l, int r) {
