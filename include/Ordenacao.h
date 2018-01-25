f64f7b3f057989cbd6436e75b89937ead9b1e408
Erick <erick_aug@hotmail.com>
2018-01-25 20:47:15 -0200

    Atualizando pai

--- include/Ordenacao.h
+++ include/Ordenacao.h
@@ -26,6 +26,15 @@ namespace ED {
             if ((fl < heapsize) && (vet[fl] > vet[pai])) imaior = fl;
             else imaior = pai;
             if ((fr < heapsize) && (vet[fr] > vet[imaior])) imaior = fr;
+			if (imaior != pai){
+                T aux = vet[pai];
+                vet[pai] = vet[imaior];
+                vet[imaior] = aux;
+                pai = imaior;
+                fl = (pai << 1) + 1;
+                fr = fl + 1;
+            }
+            else break;
 		}
 	}
 	
