6baadd70393c64eff69e828928c976a6843caaf7
Erick <erick_aug@hotmail.com>
2018-02-15 11:38:06 -0200

    Saídas do BubbleSort

--- main.cpp
+++ main.cpp
@@ -199,6 +199,12 @@ int main()
             for(k=0;k<n;k++){
                 vet[k] = gerador.nextr();
             }
+            t1 = clock();
+            ED::Ordenacao<unsigned long int>::bubbleSort(vet, n);
+            t2 = clock();
+            tempo[j] = (double) (t2-t1)/CLOCKS_PER_SEC*1000;
+            cout << "Saida " << i+1 << "." << j+1 << ": " << tempo[j] << "ms" << endl;
+            delete vet;
         }
     }
 }
