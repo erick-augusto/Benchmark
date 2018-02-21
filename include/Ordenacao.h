277cac9f5692a5311eed225e195019325fa3e0ea
Erick <erick_aug@hotmail.com>
2018-02-21 15:15:13 -0300

    Método InsertionSort

--- include/Ordenacao.h
+++ include/Ordenacao.h
@@ -69,6 +69,19 @@ namespace ED {
             heapify(vet, 0, i);
         }
     }
+	template <class T>
+	void Ordenacao<T>::insertSort(T *vet, int n){
+		int i, j, aux;
+		for(i=0;i<n;i++){
+			for(j=i;j>0;j--){
+				if(vet[j] < vet[j-1]){
+					aux = vet[j];
+					vet[j] = vet[j-1];
+					vet[j-1] = aux;
+				}
+			}
+		}
+	}
 
 	template <typename T>
     void Ordenacao<T>::quicks_central(T *vet, int l, int r) {
