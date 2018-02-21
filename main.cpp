8726515b7fc606969e7be1d1d4f0fec7035a7633
Erick <erick_aug@hotmail.com>
2018-02-21 15:20:43 -0300

    Chamando o InsertionSort

--- main.cpp
+++ main.cpp
@@ -230,4 +230,19 @@ int main()
         media[i] = (tempo[0]+tempo[1]+tempo[2]+tempo[3]+tempo[4]+tempo[5])/6;
         cout << "Media do SelectSort no caso " << i+1 << ": " << media[i] << "ms\n" << endl;
     }
+	
+	//InsertSort
+    cout << "   ***Testes do InsertSort***" << endl;
+    for(i=0;i<9;i++){
+        n = tamanho[i];
+        for(j=0;j<6;j++){
+            RandomCL<unsigned long int> gerador(i);
+            vet = new unsigned long int [n];
+            for(k=0;k<n;k++){
+                vet[k] = gerador.nextr();
+            }
+            
+        }
+    }
+    return 0;
 }
